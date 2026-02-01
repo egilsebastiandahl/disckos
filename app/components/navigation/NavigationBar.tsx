a232dd158200002022181ee244476e8e01f0e357
egilsebastiandahl <Egilsebastiandahl@outlook.com>
2026-02-01 18:36:27 +0100

    created all pages

--- app/components/navigation/NavigationBar.tsx
+++ app/components/navigation/NavigationBar.tsx
@@ -1,27 +1,85 @@
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 const NavigationBar = () => {
+  return (
+    <NavigationMenu className="font-sans">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/results"
+          >
+            Resultater
+          </NavigationMenuLink>
+        </NavigationMenuItem>
 
-    return (
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                     <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/pages/results">Resultater</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                     <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/pages/discfinder">Discfinder</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                     <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/pages/lokasjoner">Lokasjoner</NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
-        // <div className="flex gap-4 p-4">
-        //     <a href="/pages/results">Resultater</a>
-        //     <a href="/pages/discfinder">Discfinder</a>
-        //     <a href="/pages/lokasjoner">Lokasjoner</a>
-        // </div>
-    )
-}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/agenda"
+          >
+            Agenda
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/favorites"
+          >
+            Favoritter
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/discfinder"
+          >
+            DiscFinder
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/teamgenerator"
+          >
+            Lag-generator
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/videos"
+          >
+            Videoer
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/pages/lokasjoner"
+          >
+            Lokasjoner
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+    // <div className="flex gap-4 p-4">
+    //     <a href="/pages/results">Resultater</a>
+    //     <a href="/pages/discfinder">Discfinder</a>
+    //     <a href="/pages/lokasjoner">Lokasjoner</a>
+    // </div>
+  );
+};
 
 export default NavigationBar;
---
+++ app/pages/teamgenerator/page.tsx
@@ -0,0 +1,7 @@
+export default function TeamGeneratorPage() {
+  return (
+    <div>
+      <h1>Team Generator</h1>
+    </div>
+  );
+}
