b4e6b0a59ae56ee58830e326ac68cbf3a62b42a8
egilsebastiandahl <Egilsebastiandahl@outlook.com>
2026-02-02 14:31:29 +0100

    Added gender to players

--- app/types/player.model.ts
+++ app/types/player.model.ts
@@ -1,10 +1,19 @@
+import { Gender } from "./gender.enum";
+
 export interface Player {
     id: number;
     name: string;
+    gender: Gender;
+    roundsPlayed?: number;
+    averageScore?: number;
+    bestScore?: number;
+    worstScore?: number;
     singleBogeyCount?: number;
     doubleBogeyCount?: number;
     tripleBogeyCount?: number;
     parCount?: number;
     birdieCount?: number;
+    aceCount?: number;
+    eagleCount?: number;
     throws?: number;
 }
