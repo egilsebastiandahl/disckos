88f76959cab24a1f40554ea8032cc7fb357e3e66
egilsebastiandahl <Egilsebastiandahl@outlook.com>
2026-02-02 17:44:01 +0100

    updated points

--- app/components/podium/PodiumPlace.tsx
+++ app/components/podium/PodiumPlace.tsx
@@ -1,3 +1,7 @@
+import People from "@mui/icons-material/People";
+import Person from "@mui/icons-material/Person";
+import FlagIcon from "@mui/icons-material/Flag";
+
 interface PodiumPlaceProps {
   place: number;
   players: string[];
@@ -10,13 +14,29 @@ export default function PodiumPlace({
   score,
 }: PodiumPlaceProps) {
   // TODO: Add icons for players based on number of players (team or individual)
+  const firstPlaceStyle = { color: "#FFD700", fontSize: "2rem" }; // Gold
+  const secondPlaceStyle = { color: "#C0C0C0" }; // Silver
+  const thirdPlaceStyle = { color: "#CD7F32" }; // Bronze
+
+  const logoStyle =
+    place === 1
+      ? firstPlaceStyle
+      : place === 2
+        ? secondPlaceStyle
+        : thirdPlaceStyle;
+
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-12 h-12 bg-yellow-400 rounded-full flex items-center justify-center mb-2">
-        <span className="text-xl font-bold">{place}</span>
-      </div>
+    <div className={`flex flex-col items-center ${place !== 1 ? "mt-4" : ""}`}>
+      {players.length > 1 ? (
+        <People style={logoStyle} />
+      ) : (
+        <Person style={logoStyle} />
+      )}
       <span className="font-semibold">{players.join(", ")}</span>
-      <span>{score} poeng</span>
+      <span>
+        <FlagIcon />
+        {score}
+      </span>
     </div>
   );
 }
