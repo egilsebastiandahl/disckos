1c721723d61d04607ef1dd2ebad107f9e9f0a578
egilsebastiandahl <Egilsebastiandahl@outlook.com>
2026-02-10 00:16:43 +0100

    fixed mobile header

--- app/components/navigation/NavigationHeader.tsx
+++ app/components/navigation/NavigationHeader.tsx
@@ -2,26 +2,30 @@
 import Link from "next/link";
 import NavigationBar from "./NavigationBar";
 import ThemeSwitch from "@/app/features/theme_switch/ThemeSwitch";
-import { useScreenSize } from "@/app/hooks/useScreenSize";
-import { ScreenSize } from "@/app/types/screen-size.enum";
 import NavigationHeaderMobile from "./NavigationHeaderMobile";
 import NewLogo from "../logo/NewLogo";
 
 export default function NavigationHeader() {
-  const screenSize = useScreenSize();
-
   // Return Mobile header for mobile phone
-  if (screenSize == ScreenSize.XS || screenSize == ScreenSize.SM) {
-    return <NavigationHeaderMobile />;
-  }
+  // if (screenSize == ScreenSize.XS || screenSize == ScreenSize.SM) {
+  //   return <NavigationHeaderMobile />;
+  // }
 
   return (
-    <div className="sticky top-0 bg-background z-50 flex justify-between p-4 pb-2 m-auto border-b-2 border-foreground items-center">
-      <Link href={"/"} className="text-foreground text-2xl font-bold -mt-2">
-        <NewLogo />
-      </Link>
-      <NavigationBar />
-      <ThemeSwitch />
-    </div>
+    <>
+      {/* Hide if screensize is over md */}
+      <div className="md:hidden">
+        <NavigationHeaderMobile />
+      </div>
+
+      {/* Hide if screensize is under md */}
+      <div className="hidden md:flex sticky top-0 bg-background z-50 justify-between p-4 pb-2 m-auto border-b-2 border-foreground items-center">
+        <Link href={"/"} className="text-foreground text-2xl font-bold -mt-2">
+          <NewLogo />
+        </Link>
+        <NavigationBar />
+        <ThemeSwitch />
+      </div>
+    </>
   );
 }
