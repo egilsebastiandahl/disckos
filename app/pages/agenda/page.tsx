eb3716ca1779b4d58c73b149a7468ee8e5bae804
egilsebastiandahl <Egilsebastiandahl@outlook.com>
2026-02-15 13:45:36 +0100

    Updated agenda page to use public api

--- app/pages/agenda/page.tsx
+++ app/pages/agenda/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import HeaderSection from "@/app/components/sections/HeaderSection";
 import AgendaList from "./components/AgendaList";
-import eventsData from "@/app/data/events.json";
 import { Event } from "@/app/types/event.model";
+import useFetch from "@/app/hooks/useFetch";
 
 export default function AgendaPage() {
-  const events: Event[] = eventsData ?? [];
+
+  const { data: events } = useFetch<Event[]>("/api/event")
 
   const onGoToNextEventClick = () => {
     const now = new Date();
-    const nextIndex = events.findIndex((e) => new Date(e.date) > now);
+    const nextIndex = events?.findIndex((e) => new Date(e.date) > now);
     const targetIndex =
-      nextIndex !== -1 ? nextIndex : Math.max(0, events.length - 1);
+      nextIndex !== -1 ? nextIndex : 0; /* If no upcoming event, go to first event */
     const el = document.getElementById(`agenda-item-${targetIndex}`);
     if (el) el.scrollIntoView({ behavior: "smooth", block: "center" });
   };
@@ -24,7 +25,7 @@ export default function AgendaPage() {
         buttonClick={onGoToNextEventClick}
       />
       <main className="flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 justify-center">
-        <AgendaList events={events} />
+        <AgendaList events={events ?? []} />
       </main>
     </>
   );
