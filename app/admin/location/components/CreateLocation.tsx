8da92727718838da4f9a6a99c60bd5366606ddce
egilsebastiandahl <Egilsebastiandahl@outlook.com>
2026-02-15 16:49:21 +0100

    Opprett ny lokasjon med labels

--- app/admin/location/components/CreateLocation.tsx
+++ app/admin/location/components/CreateLocation.tsx
@@ -40,19 +40,27 @@ export default function CreateLocation() {
                 setName("")
                 setDescription("")
                 setLat(0)
+                setLatString("0")
                 setLon(0)
+                setLonString("0")
             } else {
                 alert("Failed to create location.")
             }
         })
     }
 
-    return (<div className="flex flex-col max-w-sm p-16">
+    return (<div className="flex flex-col w-full items-center justify-center p-16 gap-8">
         <Input type="text" placeholder="Navn" value={name} onChange={(e) => setName(e.target.value)} />
         <Input type="text" placeholder="Beskrivelse" value={description} onChange={(e) => setDescription(e.target.value)} />
-        <Input type="number" placeholder="Latitude" value={latString} onChange={(e) => onChangeLatString(e.target.value)} />
-        <Input type="number" placeholder="Longitude" value={lonString} onChange={(e) => onChangeLonString(e.target.value)} />
-        <Button onClick={onCreateClick}>Lag</Button>
+        <section className="flex flex-row gap-4 w-full items-center">
+            <label htmlFor="latInput" className="text-sm">Latitude</label>
+            <Input id="latInput" type="number" placeholder="Latitude" value={latString} onChange={(e) => onChangeLatString(e.target.value)} />
+        </section>
+        <section className="flex flex-row gap-4 w-full items-center">
+            <label htmlFor="lonInput" className="text-sm">Longitude</label>
+            <Input id="lonInput" type="number" placeholder="Longitude" value={lonString} onChange={(e) => onChangeLonString(e.target.value)} />
+        </section>
+        <Button onClick={onCreateClick}>Opprett</Button>
     </div>)
 
 
